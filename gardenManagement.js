958d99d9e4dec256c17193264ab0210c39873b93
Ndukuyo <harrisonndukuyo@gmail.com>
2025-11-16 21:51:43 +0300

    i have updated the gardenManagement.js file

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,21 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80) {
+    console.log("watering on");
+} else {
+    console.log("watering off");
+}
+
+if (timeOfDay === "evening" || timeOfDay === "night"){
+    console.log("Lights on")
+} else {
+    console.log("Lights off")
+}
+
+while (soilMoisture <40){
+    console.log(soilMoisture);
+    soilMoisture += 5;
+}
+console.log(soilMoisture);
